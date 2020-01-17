ffb497af5424a35ccb30ba2ed1c5d14442742669
beeeeeeep <jeremy.gigase@gmail.com>
2020-01-17 22:37:56 +0100

    Working version 1.0

--- mastermind.js
+++ mastermind.js
@@ -1,9 +1,16 @@
 
-// Initialisation variables and let's
+// Setting variables and let's
 let colors = ["red", "blue", "yellow", "green"];
 let row_number = 1;
 let clicked = 0;
-var oplossing = [];
+var solution = [];
+var user_solution = []
+
+// Initialisation of the solution
+initialisation()
+function initialisation () {
+    random1_4();
+}
 
 // Returns the second class of an element
 function get2ndClass(element) {
@@ -48,18 +55,23 @@ function setBGColor (element , color_number){
 function newRow (){
     row_number++;
     let div = document.createElement("div");
-    div.id = "rij_" + row_number;
+    let row = "rij_" + row_number;
+    div.id = row
     document.getElementById("rijen").appendChild(div);
-
+    user_solution = [];
 
     let circle_number;
     for (circle_number = 1; circle_number < 5; circle_number++) {
         let circle = "g1_" + circle_number;
         let element = document.getElementById(circle);
         let color = get2ndClass(element);
+        user_solution.push(color);
         createCircle(row_number, circle_number, color)
     }
 
+    console.log(user_solution)
+
+    appendContent(row)
 }
 
 
@@ -79,44 +91,99 @@ function random1_4(){
 
     for (i = 1; i < 5; i++) {
         var color_number = Math.floor(Math.random() * 4);
-        oplossing.push (colors[color_number]);
+        solution.push (colors[color_number]);
     }
-    return oplossing
-    //console.log(oplossing)
+    return solution
 }
 
 // Toggle between showing the solution and not showing it
 function toonOplossing () {
 
-    // document.write(oplossing)
+
     if (clicked == 1) {
-        //document.getElementById("message").innerHTML = "Already showed solution!";
-        for (i = 0; i < 4; i++) {
+            // Not show
+        for (i = 0; i < 4; i++)
+        {
             let circle_number = i + 1
             let circle = "sol" + circle_number;
             let element = document.getElementById(circle);
             let secondClass = get2ndClass(element)
             element.classList.remove(secondClass);
-            //document.getElementById("message").style.display="block"
         }
         clicked--
-    } else {
-        var oplossing = random1_4();
-        for (i = 0; i < 4; i++) {
-            let circle_number = i + 1
-            let circle = "sol" + circle_number;
-            let element = document.getElementById(circle);
-            element.classList.add(oplossing[i]);
-            //document.write(oplossing[i])
-        }
-        clicked++
+    } else
+        {
+            // Show
+            showSolution();
+            clicked++
     }
 }
 
-function appendContent( el_name, new_content ) {
+function showSolution () {
+    for (i = 0; i < 4; i++) {
+        let circle_number = i + 1
+        let circle = "sol" + circle_number;
+        let element = document.getElementById(circle);
+        element.classList.add(solution[i]);
+    }
 
 }
 
-function feedback( black, white ) {
+function appendContent( el_name) {
+
+    let white = 0;
+    let black = 0;
+    let test_solution = solution.slice()
+    for (i = 0; i < 4; i++) {
+
+        if (test_solution[i] == user_solution[i]){
+            black++
+            user_solution[i] = "black"
+            test_solution[i] = "null"
+        }
+    }
+
+    for (i = 0; i < 4; i++) {
+
+        if (test_solution.includes(user_solution[i])){
+            white++
+            let remove_color = user_solution[i]
+            let remove_color_number = test_solution.indexOf(remove_color)
+            user_solution[i] = "white"
+            test_solution[remove_color_number] = "null"
+        }
+    }
+
+    console.log(user_solution)
+    console.log(solution)
+    feedback(black, white, el_name);
+
+}
+
+function feedback( black, white, el_name ) {
+
+    if (black == 4) {
+
+        for (i = 0; i < black; i++) {
+            createDot("black", el_name)
+        }
+        showSolution()
+        alert("You've won!")
+    }
+        else {
+            for (i = 0; i < black; i++) {
+                createDot("black", el_name)
+            }
+            for (i = 0; i < white; i++) {
+                createDot("white", el_name)
+            }
+        }
+}
+
+function createDot (color , el_name) {
+
+    let span= document.createElement("span");
+    span.className = color;
+    document.getElementById(el_name).appendChild(span);
 
 }
